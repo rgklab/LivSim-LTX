a8f5d6e334210d61247640125ae673b9ad7f3d72
Xiang Gao <xgao@cs.toronto.edu>
2023-11-09 01:42:40 -0500

    add delta models

---
+++ LivSim_Processing/run_delta.sh
@@ -0,0 +1,4 @@
+for i in DynaMELD_MELD3.0_First DynaMELD_MELD3.0_First_Second DynaMELD_Biological_First DynaMELD_Biological_First_Second DynaMELD_Common_First DynaMELD_Common_First_Second
+do
+    python simulate.py 5 1 $i
+done
